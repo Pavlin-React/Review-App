a94b1ad6e4ef4e8f403c97d24624059aaa281f62
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-05-20 09:16:56 +0300

    Navigation setup

---
+++ .history/screens/Home_20210520090016.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global'
+
+const Home = () => {
+  return (
+    <View style={ globalStyles.container }>
+      <Text style={ globalStyles.titleText }>Home Screennn</Text>
+    </View>
+  );
+}
+ 
+export default Home;
